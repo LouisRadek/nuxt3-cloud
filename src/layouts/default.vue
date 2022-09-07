d0c1959da95b3d18ceef7211e9a54302eb1c31e2
Louis Radek <radek.louis@gmail.com>
2022-09-07 17:24:00 +0200

    feat: add navbar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,7 @@
   export default {
     data() {
       return {
-        isDarkThemeActive: false
+        isDarkThemeActive: true
       }
     },
 
@@ -16,13 +16,37 @@
 
 <template>
   <VApp :theme="isDarkThemeActive ? 'dark' : 'light'">
-    <div class="d-flex flex-row justify-end">
-      <VBtn 
-        @click="toggleTheme" 
-        icon="mdi-brightness-6"
-        size="large"
+    <VAppBar>
+      <VAppBarNavIcon />
+
+      <VAppBarNavIcon 
+        icon="mdi-magnify"
       />
-    </div>
+
+      <VTextField 
+        class="SearchField"
+        density="compact"
+        placeholder="search in your files"
+        variant="outlined"
+      />
+
+      <VSpacer />
+
+      <VAppBarNavIcon 
+        icon="mdi-brightness-6"
+        @click="toggleTheme" 
+      />
+
+      <VAppBarNavIcon 
+        icon="mdi-logout"
+      />
+    </VAppBar>
     <slot />
   </VApp>
 </template>
+
+<style scoped>
+.SearchField :deep(.v-field__field) {
+  border-radius: 50%;
+}
+</style>
