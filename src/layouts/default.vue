937279856be1eede8541f1844db1d2c7e40302e5
Louis Radek <radek.louis@gmail.com>
2022-09-07 15:34:41 +0200

    feat: add finished login page

---
+++ src/layouts/default.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    data() {
+      return {
+        isDarkThemeActive: false
+      }
+    },
+
+    methods: {
+      toggleTheme() {
+        this.isDarkThemeActive = !this.isDarkThemeActive  
+      }
+    }
+  }
+</script>
+
+<template>
+  <VApp :theme="isDarkThemeActive ? 'dark' : 'light'">
+    <div class="d-flex flex-row justify-end">
+      <VBtn 
+        @click="toggleTheme" 
+        icon="mdi-brightness-6"
+        size="large"
+      />
+    </div>
+    <slot />
+  </VApp>
+</template>
