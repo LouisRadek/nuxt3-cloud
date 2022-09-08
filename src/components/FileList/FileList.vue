fe7c71b8eb96343c7dc6a0a41a3fb137721c9b82
Louis Radek <radek.louis@gmail.com>
2022-09-08 18:07:40 +0200

    feat: add component-structure and plus-btn

---
+++ src/components/FileList/FileList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    files: {
+      type: Array,
+      required: true
+    }
+  },
+
+  methods: {
+    onDelete (id) {
+      this.$emit('delete', id)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <FileListItem
+      v-for="file in files"
+      :key="file.id"
+      :file="file"
+      @delete="onDelete(file.id)"
+    />
+  </div>
+  <VBtn @click="onDelete('test')">click me</VBtn>
+</template>
---
+++ src/components/FileList/FileListItem.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+  props: {
+    file: {
+      type: Object,
+      required: true,
+      validator(file) {
+        return (
+          file.id !== undefined &&
+          file.name !== undefined &&
+          file.type !== undefined
+        )
+      }
+    }
+  },
+
+  methods: {
+    onClickDelete () {
+      this.$emit('delete')
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    {{ file.name }}
+
+    <VBtn @click="onClickDelete">delete {{ file.name }}</VBtn>
+  </div>
+</template>
